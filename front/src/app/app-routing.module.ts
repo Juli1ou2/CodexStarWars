8d7b39e2e9b430702fefdf00ebd933890b3568ef
julien <julien.mrss@gmail.com>
2024-04-26 19:02:40 +0200

    feat(front): add border-container component and updates (components, index and routes)

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
+import { VehiculesComponent } from './vehicules/vehicules.component';
 
 const routes: Routes = [
+  { path: 'vehicules', component: VehiculesComponent },
   { path: 'accueil', component: AccueilComponent },
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
 ];
